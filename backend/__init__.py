103057c28558beedeb306716d5744e027ad42133
Lucas Lopes <98142825+ohucas@users.noreply.github.com>
2025-09-24 14:22:11 -0300

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,2 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    # ... configure seu aplicativo ...
+    db.init_app(app)
+    
+    from . import models  # Isso importará os modelos
+    from . import routes
+    
+    return app
 
